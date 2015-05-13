e20a76bb2932474251d78df592d26005e3d9340c
Suriyadeepan Ram <suriyadeepan@acm.org>
2015-05-13 17:54:08 +0530

    About Page Complete : Cover Photo should be changed

--- app/src/main/java/org/fsftn/fsconf15/AboutActivity.java
+++ app/src/main/java/org/fsftn/fsconf15/AboutActivity.java
@@ -2,8 +2,10 @@ package org.fsftn.fsconf15;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class AboutActivity extends ActionBarActivity {
@@ -12,6 +14,20 @@ public class AboutActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView contribList = (TextView) findViewById(R.id.ccontent);
+        contribList.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView contribAr = (TextView) findViewById(R.id.contrib_aravinth);
+        contribAr.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView contribRa = (TextView) findViewById(R.id.contrib_rajanand);
+        contribRa.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView contribSu = (TextView) findViewById(R.id.contrib_suriya);
+        contribSu.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
+
+
     }
 
     @Override
@@ -36,3 +52,14 @@ public class AboutActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+/*
+
+1. build version
+2. Repo URL
+3. License info (inface we have to include GPL license file in the repo itself)
+4. Organization Name
+5. Credits
+
+
+ */
